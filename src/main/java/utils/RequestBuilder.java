63c805ecdae159fefcf0675f20d1e94e1240be09
엄태호 <qqsscc7342@gmail.com>
2023-02-03 14:31:42 +0900

    feat: 쿼리스트링 파싱 구현

--- src/main/java/utils/RequestBuilder.java
+++ src/main/java/utils/RequestBuilder.java
@@ -5,6 +5,7 @@ import webserver.HttpRequest;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,6 +14,7 @@ public class RequestBuilder {
     public HttpRequest getHttpRequest(BufferedReader bufferedReader) throws IOException {
         String line = "";
         String requestPath = "";
+        Map<String, String> queryString = new HashMap<>();
         String httpMethod = "";
         String httpProtocol = "";
 
@@ -23,13 +25,21 @@ public class RequestBuilder {
 
             if (!line.contains(":")) {
                 httpMethod = tokens[0];
-                requestPath = tokens[1];
+                requestPath = tokens[1].split("\\?")[0];
+
+                if (tokens[1].contains("?")) {
+                    String[] params = tokens[1].split("\\?")[1].split("&");
+                    Arrays.stream(params)
+                            .map(param -> param.split("="))
+                            .forEach((split) -> queryString.put(split[0], split[1]));
+                }
+
                 httpProtocol = tokens[2];
 
                 continue;
             }
             requestHeaderMap.put(tokens[0].substring(0, tokens[0].length() - 1), tokens[1]);
         }
-        return new HttpRequest(httpMethod, requestPath, httpProtocol, requestHeaderMap);
+        return new HttpRequest(httpMethod, requestPath, queryString, httpProtocol, requestHeaderMap);
     }
 }
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -11,6 +11,7 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
