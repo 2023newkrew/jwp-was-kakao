f0d64e55ddb625e0573f465be6237f2429e97a49
HQkim <hyunkyu001@gmail.com>
2023-02-03 16:40:01 +0900

    feat: "GET /css/styles.css" 요청에 대한 응답 테스트 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -7,13 +7,15 @@ import utils.FileIoUtils;
 import java.io.*;
 import java.net.Socket;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private final Socket connection;
+
+    private final Map<String, String> headers = new HashMap<>();
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -27,29 +29,34 @@ public class RequestHandler implements Runnable {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             InputStreamReader reader = new InputStreamReader(in);
             BufferedReader br = new BufferedReader(reader);
-            List<String> lines = new ArrayList<>();
             String line = br.readLine();
-            while (!"".equals(line)) {
-                if (line == null) { return; }
-                System.out.println(line);
-                lines.add(line);
-                line = br.readLine();
-            }
 
-            if (lines.size() == 0) {
+            if (line == null || "".equals(line)) {
                 throw new RuntimeException("잘못된 요청 형식입니다.");
             }
 
-            String[] tokens = lines.get(0).split(" ");
+            String[] tokens = line.split(" ");
             String url = tokens[1];
+
+            line = br.readLine();
+
+            while (!"".equals(line)) {
+                if (line == null) { return; }
+                System.out.println(line);
+                String[] header = line.split(": ");
+                headers.put(header[0], header[1]);
+                line = br.readLine();
+            }
 
             DataOutputStream dos = new DataOutputStream(out);
 
             byte[] body;
             if (url.equals("/")) {
                 body = "Hello world".getBytes();
-            } else {
+            } else if (url.equals("/index.html")) {
                 body = FileIoUtils.loadFileFromClasspath("./templates" + url);
+            } else {
+                body = FileIoUtils.loadFileFromClasspath("./static" + url);
             }
 
             response200Header(dos, body.length);
@@ -66,7 +73,8 @@ public class RequestHandler implements Runnable {
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8 \r\n");
+            String contentType = headers.getOrDefault("Accept", "text/html").split(",")[0];
+            dos.writeBytes("Content-Type: " + contentType + ";charset=utf-8 \r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + " \r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
