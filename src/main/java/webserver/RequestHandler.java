86cdd553c8fa888ff2e4610d4470530f0a393a8d
엄태호 <qqsscc7342@gmail.com>
2023-02-03 13:20:02 +0900

    feat: Request Header 읽어오기 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,11 +3,9 @@ package webserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -22,9 +20,16 @@ public class RequestHandler implements Runnable {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
+        try (InputStream in = connection.getInputStream();
+             OutputStream out = connection.getOutputStream();
+             DataOutputStream dos = new DataOutputStream(out);
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8))) {
+
+            String line = "";
+            while (hasNextHeader(line = bufferedReader.readLine())) {
+                logger.debug("line : {}", line);
+            }
+
             byte[] body = "Hello world".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
@@ -33,6 +38,9 @@ public class RequestHandler implements Runnable {
         }
     }
 
+    private boolean hasNextHeader(String line) {
+        return line != null && !line.equals("");
+    }
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
