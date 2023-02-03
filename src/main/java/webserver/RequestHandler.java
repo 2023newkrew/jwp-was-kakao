45732c6ce3fb6b8873f04d8b13578c644f473504
HQkim <hyunkyu001@gmail.com>
2023-02-03 15:26:04 +0900

    feat: "GET /index.html" 요청에 대한 응답 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,17 +2,18 @@ package webserver;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.FileIoUtils;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -24,12 +25,41 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            InputStreamReader reader = new InputStreamReader(in);
+            BufferedReader br = new BufferedReader(reader);
+            List<String> lines = new ArrayList<>();
+            String line = br.readLine();
+            while (!"".equals(line)) {
+                if (line == null) { return; }
+                System.out.println(line);
+                lines.add(line);
+                line = br.readLine();
+            }
+
+            if (lines.size() == 0) {
+                throw new RuntimeException("잘못된 요청 형식입니다.");
+            }
+
+            String[] tokens = lines.get(0).split(" ");
+            String url = tokens[1];
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello world".getBytes();
+
+            byte[] body;
+            if (url.equals("/")) {
+                body = "Hello world".getBytes();
+            } else {
+                body = FileIoUtils.loadFileFromClasspath("./templates" + url);
+            }
+
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            logger.error(e.getMessage());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
     }
 
