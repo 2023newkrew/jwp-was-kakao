5cc3869182977dbebd91191af0ebb3de84e36e3e
jade.xyz <jade.xyz@kakaocorp.com>
2023-02-03 15:03:01 +0900

    feat: socket input 값 파싱

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,11 +3,10 @@ package webserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,7 +22,17 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            InputStreamReader inputStreamReader = new InputStreamReader(in);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            List<String> requestStrings = new ArrayList<>();
+            String s = bufferedReader.readLine();
+            while (s != null) {
+                requestStrings.add(s);
+                s = bufferedReader.readLine();
+            }
+            Request request = RequestParser.parse(requestStrings);
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello world".getBytes();
             response200Header(dos, body.length);
