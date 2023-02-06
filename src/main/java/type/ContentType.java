52e0bade257c685447ee4364d8af52782e156ab3
MibSang <secured8372@gmail.com>
2023-02-06 16:58:39 +0900

    Revert "fix: 페이지에 아이콘이 깨지던 문제 수정"
    This reverts commit 79778f19cf3afda5aad04ac1aa6db44e42e1a645.

--- src/main/java/type/ContentType.java
+++ src/main/java/type/ContentType.java
@@ -5,9 +5,9 @@ public enum ContentType {
     CSS("text/css;charset=utf-8"),
     JS("application/javascript;charset=utf-8"),
     WOFF("font/woff"),
-    WOFF2("font/woff2"),
     TTF("application/x-font-ttf"),
-    ICO("image/x-icon");
+    ICO("image/x-icon"),
+    ;
 
     private final String string;
 
