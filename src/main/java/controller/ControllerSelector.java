e3fc1fb7e21cf86dcd55df9e5201fae35fe6823f
jade.xyz <jade.xyz@kakaocorp.com>
2023-02-06 17:24:03 +0900

    feat: createUser api 엔드포인트 변경

--- src/main/java/controller/ControllerSelector.java
+++ src/main/java/controller/ControllerSelector.java
@@ -4,9 +4,13 @@ import dto.BaseResponseDto;
 import webserver.Request;
 import webserver.StatusCode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import static webserver.HttpMethod.GET;
+import static webserver.HttpMethod.POST;
 
 public class ControllerSelector {
 
@@ -20,12 +24,25 @@ public class ControllerSelector {
             return new BaseResponseDto(StatusCode.OK, baseController.hello());
         }
         // userController
-        if (request.getHttpMethod() == GET
+        if (request.getHttpMethod() == POST
                 && Objects.equals(request.getUrl(), "/user/create")) {
+            Map<String, String> requestBody = extractBody(request.getBody());
+
             return new BaseResponseDto(StatusCode.CREATED,
-                    userController.createUser(request.getQueryParams()));
+                    userController.createUser(requestBody));
         }
 
         return new BaseResponseDto(StatusCode.NOT_FOUND, "");
+    }
+
+    private static Map<String, String> extractBody(String rawBody) {
+        Map<String, String> body = new HashMap<>();
+        Arrays.stream(rawBody.split("&"))
+                .forEach(v -> {
+                    String[] kv = v.split("=");
+                    body.put(kv[0], kv[1]);
+                });
+
+        return body;
     }
 }
