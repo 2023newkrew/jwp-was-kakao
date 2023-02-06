e807fb7b3491ff7f75d912ed3a0cd81ac3c43541
MibSang <secured8372@gmail.com>
2023-02-06 16:13:27 +0900

    refactor: Response 객체 빌더패턴 도입

--- src/main/java/webserver/Response.java
+++ src/main/java/webserver/Response.java
@@ -1,35 +1,89 @@
 package webserver;
 
+import type.ContentType;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Response {
 
     private final String header;
 
     private final String body;
 
-    public Response(String header, String body) {
-        this.header = header +
-        "Content-Length: " + body.getBytes().length + " ";
+    private Response(String header, String body) {
+        this.header = header;
         this.body = body;
     }
 
-    public Response(String header) {
-        this.header = header +
-                "Content-Length: 0 ";
+    private Response(String header) {
+        this.header = header;
         this.body = null;
     }
 
-    public byte[] toBytes() {
-        if (body == null) {
-            return header.getBytes();
+    public static class ResponseBuilder {
+        private final String status;
+        private String contentLength;
+        private String contentType;
+        private String location;
+        private String body;
+
+        public ResponseBuilder(String status) {
+            this.status = status;
         }
-        return (header + "\r\n\r\n" + body).getBytes();
+
+        private void contentLength(int length) {
+            this.contentLength = "Content-Length: " + length;
+        }
+
+        public ResponseBuilder contentType(ContentType contentType) {
+            this.contentType = "Content-Type: " + contentType.getString();
+            return this;
+        }
+
+        public ResponseBuilder location(String uri) {
+            this.location = "Location: " + uri;
+            return this;
+        }
+
+        public ResponseBuilder body(String body) {
+            this.body = body;
+            contentLength(this.body.getBytes().length);
+            return this;
+        }
+
+        public Response build() {
+            if (body == null) {
+                contentLength(0);
+            }
+            String header = Stream.of(status, contentType, location, contentLength)
+                    .filter(s -> s != null && !s.isEmpty())
+                    .collect(Collectors.joining(" \r\n")) + " ";
+            if (body == null) {
+                return new Response(header);
+            }
+            return new Response(header, body);
+        }
+
     }
 
-    public String getBody() {
-        return body;
+    public static ResponseBuilder ok() {
+        return new ResponseBuilder("HTTP/1.1 200 OK");
     }
 
-    public String getHeader() {
-        return header;
+    public static ResponseBuilder notFound() {
+        return new ResponseBuilder("HTTP/1.1 404 Not found");
+    }
+
+    public static ResponseBuilder redirect() {
+        return new ResponseBuilder("HTTP/1.1 302 Redirect");
+    }
+
+    @Override
+    public String toString() {
+        if (body == null) {
+            return header;
+        }
+        return header + "\r\n\r\n" + body;
     }
 }
