3bf614458b7cab1ed6fdceb6f5a3a0183a3129b9
eddie.kk <eddie.kk@kakaocorp.com>
2023-02-06 14:19:02 +0900

    feat: HttpRequest 헤더 쿼리 스트링 파싱 구현

--- src/main/java/http/HttpUrl.java
+++ src/main/java/http/HttpUrl.java
@@ -1,14 +1,34 @@
 package http;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class HttpUrl {
 
     private String path;
+    private Map<String, String> params;
 
     public HttpUrl(String path) {
         this.path = path;
+        this.params = parseParams(path);
     }
 
     public String getPath() {
         return path;
+    }
+    
+    private Map<String, String> parseParams(String path) {
+        if (!path.contains("?")) {
+            return new HashMap<>();
+        }
+
+        String queryString = path.substring(path.indexOf("?") + 1);
+        String[] parameters = queryString.split("&");
+
+        return Arrays.stream(parameters)
+                .map(parameter -> parameter.split("="))
+                .collect(Collectors.toMap(parameter -> parameter[0], parameter -> parameter[1]));
     }
 }
