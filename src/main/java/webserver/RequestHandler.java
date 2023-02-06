8c1818c28934b76414fb3f3b82a2e0231b6130ea
kakao <coolofficials@gmail.com>
2023-02-06 16:40:59 +0900

    feat: CSS support
    resolved: #4

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URISyntaxException;
@@ -28,18 +30,41 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = FileIoUtils.loadFileFromClasspath("templates/index.html");
-            response200Header(dos, body.length);
+            String request = new BufferedReader(new InputStreamReader(in)).readLine();
+            String requestPath = request.split(" ")[1];
+            byte[] body = new byte[0];
+            String contentType = "";
+            if (requestPath.equals("/index.html")) {
+                body = FileIoUtils.loadFileFromClasspath("templates/index.html");
+                contentType = "text/html;charset=utf-8";
+            }
+            else if (requestPath.startsWith("/css")) {
+                body = FileIoUtils.loadFileFromClasspath("static" + requestPath);
+                contentType = "text/css";
+            }
+            else if (requestPath.startsWith("/js")) {
+                body = FileIoUtils.loadFileFromClasspath("static" + requestPath);
+                contentType = "application/javascript;charset=utf-8";
+            }
+            else if (requestPath.startsWith("/images")) {
+                body = FileIoUtils.loadFileFromClasspath("static" + requestPath);
+                contentType = "images/*";
+            }
+            else if (requestPath.startsWith("/fonts")) {
+                body = FileIoUtils.loadFileFromClasspath("static" + requestPath);
+                contentType = "font/*";
+            }
+            response200Header(dos, body.length, contentType);
             responseBody(dos, body);
         } catch (IOException|URISyntaxException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8 \r\n");
+            dos.writeBytes("Content-Type: " + contentType + " \r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + " \r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
