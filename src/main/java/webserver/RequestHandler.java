4cc37962059d67652f8a9bf692c0d09578f685be
SJGeeKo <sungjun.gee.ko@gmail.com>
2023-02-06 18:51:39 +0900

    refactor: ResponseHeader 클래스를 이용해 RequestHandler 코드 중복 제거

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,16 +5,18 @@ import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import type.ContentType;
+import type.HttpStatusCode;
 import utils.FileIoUtils;
 
 import java.io.*;
 import java.net.Socket;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Map;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String PROTOCOL = "HTTP";
+    private static final String VERSION = "1.1";
 
     private Socket connection;
 
@@ -26,24 +28,21 @@ public class RequestHandler implements Runnable {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            List<String> roots = List.of("templates", "static");
+        try (InputStream in = connection.getInputStream();
+             OutputStream out = connection.getOutputStream()
+        ) {
             InputStreamReader inputStreamReader = new InputStreamReader(in);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            RequestParser requestParser = new RequestParser(bufferedReader);
 
+            RequestParser requestParser = new RequestParser(bufferedReader);
             RequestHeader header = requestParser.getRequestHeader();
+            //TODO: RequestHeader에 URI가 포함되지 않은 상황을 고려해야 할까?
+            String uri = header.get("URI").orElseThrow(IllegalArgumentException::new);
 
             DataOutputStream dos = new DataOutputStream(out);
-            //TODO: RequestHeader에 URI가 포함되지 않은 상황을 고려해야 할까?
-            byte[] returnBody = null;
-            String uri = header.get("URI").orElseThrow(RuntimeException::new);
 
-            String[] uriSplit = uri.split("\\?");
-            String uriWithOutParams = uriSplit[0];
-
-            if (uriWithOutParams.equals("/user/create")) {
+            // 유저 등록 상황
+            if (uri.startsWith("/user/create")) {
                 User user = new User(
                         requestParser.getParam("userId"),
                         requestParser.getParam("password"),
@@ -51,68 +50,37 @@ public class RequestHandler implements Runnable {
                         requestParser.getParam("email")
                 );
                 DataBase.addUser(user);
-                response302Header(dos);
+                // 생성 후 index로 redirect
+                dos.writeBytes(ResponseHeader.of(HttpStatusCode.REDIRECT, "/index.html").getValue());
                 responseBody(dos, new byte[0]);
                 return;
             }
+
+            List<String> roots = List.of("templates", "static");
+            byte[] returnBody = null;
 
             for (String root : roots) {
                 returnBody = FileIoUtils.loadFileFromClasspath(root + uri);
                 if (returnBody != null) {
                     String[] split = uri.split("\\.");
-                    String ext = split[split.length - 1];
-                    response200Header(dos, returnBody.length, ContentType.valueOf(ext.toUpperCase()));
+                    String fileType = split[split.length - 1];
+                    dos.writeBytes(
+                            ResponseHeader.of(HttpStatusCode.OK,
+                                            ContentType.valueOf(fileType.toUpperCase()),
+                                            returnBody.length)
+                                    .getValue()
+                    );
                     break;
                 }
             }
             if (returnBody == null) {
-                response404Header(dos);
+                dos.writeBytes(ResponseHeader.of(HttpStatusCode.NOT_FOUND, ContentType.HTML).getValue());
             }
             responseBody(dos, returnBody);
         } catch (IOException e) {
             logger.error(e.getMessage());
         } catch (URISyntaxException e) {
             return;
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, ContentType type) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: " + type.getToResponseText() + " \r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + " \r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void response201Header(DataOutputStream dos) {
-        try {
-            dos.writeBytes("HTTP/1.1 201 CREATED \r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void response302Header(DataOutputStream dos) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 REDIRECT \r\n");
-            dos.writeBytes("Location: /index.html");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void response404Header(DataOutputStream dos) {
-        try {
-            dos.writeBytes("HTTP/1.1 404 NOT FOUND \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8 \r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
         }
     }
 
