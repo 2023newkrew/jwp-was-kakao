2e35124570760fcc745a8a35de7878831e3b9865
kjaehyeon <33208296+kjaehyeon@users.noreply.github.com>
2023-02-07 17:22:23 +0900

    [jacob.hyeon] 웹 서버 구현 1단계 리뷰요청 드립니다! (#13)
    * feat: pathParser 생성, index.html 연결
    
    * feat: html 이외의 확장자 적용
    
    * feat: 회원가입 기능 추가
    
    * feat: 회원가입 후 리다이렉트 기능 추가
    
    * refactor: 리팩토링 진행
    - 응답 header 생성 메서드 ResponseUtils 로 옮김
    - 요청 header 읽기 메서드 IOUtils 로 옮김
    - http 메서드별 스위치문 작성
    
    * refactor: 요청 파싱할때 HttpMethod enum 반환하도록 변경
    
    * feat: Query String 파싱 지원
    
    ---------
    
    Co-authored-by: mkom76 <mkom76@yonsei.ac.kr>
    Co-authored-by: jacob-hyeon <jacob.hyeon@kakaocorp.com>

---
+++ src/main/java/utils/HttpParser.java
@@ -0,0 +1,38 @@
+package utils;
+
+import org.springframework.http.HttpMethod;
+
+import java.util.HashMap;
+
+public class HttpParser {
+    private final HashMap<String, String> httpHeaderInfo = new HashMap<>();
+
+    public HttpParser(String httpRequest) {
+        //System.out.println(httpRequest);
+
+        String[] httpInfo = httpRequest.split("\n");
+        String[] firstLine = httpInfo[0].split(" ");
+
+        httpHeaderInfo.put("Http-Method", firstLine[0]);
+        httpHeaderInfo.put("Path", firstLine[1]);
+        httpHeaderInfo.put("Http-Version", firstLine[2]);
+
+        for (int i = 1; i < httpInfo.length; i++) {
+            String key = httpInfo[i].split(": ")[0];
+            String value = httpInfo[i].split(": ")[1];
+            httpHeaderInfo.put(key, value);
+        }
+    }
+
+    public HttpMethod getHttpMethod() {
+        return HttpMethod.resolve(httpHeaderInfo.get("Http-Method"));
+    }
+
+    public String getPath() {
+        return httpHeaderInfo.get("Path");
+    }
+
+    public Integer getContentLength(){
+        return Integer.parseInt(httpHeaderInfo.get("Content-Length"));
+    }
+}
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,55 +1,92 @@
 package webserver;
 
+import db.DataBase;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.FileIoUtils;
+import utils.HttpParser;
+import utils.IOUtils;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.net.URISyntaxException;
+import java.util.HashMap;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String TEMPLATE_ROOT_PATH = "./templates";
+    private static final String STATIC_ROOT_PATH = "./static";
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
 
     public void run() {
-        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
-
+        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello world".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
+
+            HttpParser httpParser = new HttpParser(IOUtils.readHeader(br));
+            String path = httpParser.getPath();
+            byte[] body = new byte[0];
+            switch (httpParser.getHttpMethod()){
+                case POST:
+                    Integer contentLength = httpParser.getContentLength();
+                    HashMap<String, String> requestBody = parseQueryString(IOUtils.readData(br, contentLength));
+
+                    if(path.startsWith("/user/create")){
+                        User user = new User(requestBody.get("userId"), requestBody.get("password"), requestBody.get("name"), requestBody.get("email"));
+                        DataBase.addUser(user);
+                        System.out.println("[USER CREATED] - " + DataBase.findUserById(user.getUserId()));
+                        ResponseUtils.response302Header(dos, "/index.html");
+                    }else{
+                        ResponseUtils.response404Header(dos);
+                    }
+                    break;
+
+                case GET:
+                    HashMap<String, String> queryParam = null;
+                    if(path.contains("?")) queryParam = parseQueryString(path.substring(path.indexOf('?') + 1));
+
+                    if(path.equals("/")){
+                        ResponseUtils.response302Header(dos, "/index.html");
+                    }else if(path.endsWith(".html") || path.endsWith("/favicon.ico")){
+                        body = FileIoUtils.loadFileFromClasspath(TEMPLATE_ROOT_PATH + path);
+                        ResponseUtils.response200Header(dos, body.length, path);
+                    }else if(path.startsWith("/css") || path.startsWith("/fonts") || path.startsWith("/images") || path.startsWith("/js")){
+                        body = FileIoUtils.loadFileFromClasspath(STATIC_ROOT_PATH + path);
+                        ResponseUtils.response200Header(dos, body.length, path);
+                    }else{
+                        ResponseUtils.response404Header(dos);
+                    }
+                    break;
+
+                case PUT:
+                case DELETE:
+                    ResponseUtils.response404Header(dos);
+                    break;
+
+                default:
+                    ResponseUtils.response400Header(dos);
+            }
+            ResponseUtils.responseBody(dos, body);
+        } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8 \r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + " \r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
+    private HashMap<String, String> parseQueryString(String requestBody){
+        HashMap<String, String> result = new HashMap<>();
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
+        for(String info : requestBody.split("&")){
+            String key = info.split("=")[0];
+            String value = info.split("=")[1];
+            result.put(key, value);
         }
+        return result;
     }
 }
