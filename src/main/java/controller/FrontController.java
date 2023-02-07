e0326c401ea5ef84e1e0c3ba243a9ae6f6a5f438
jerrie.choi <pinkytime96@gmail.com>
2023-02-07 17:21:07 +0900

    refac: 요청 객체를 controller에서 원하는 타입으로 매핑
    - RequestBody와 RequestParams 어노테이션 사용 시에만 적용
    - 커스텀 예외(UnsupportedRequestException) 추가

--- src/main/java/controller/FrontController.java
+++ src/main/java/controller/FrontController.java
@@ -1,13 +1,21 @@
 package controller;
 
+import controller.annotation.CustomRequestBody;
 import controller.annotation.CustomRequestMapping;
-import model.CustomHttpRequest;
-import model.CustomHttpResponse;
+import controller.annotation.CustomRequestParams;
+import exception.UnsupportedRequestException;
+import exception.UnsupportedResponseException;
+import model.http.CustomBaseHttpRequest;
+import model.http.CustomHttpRequest;
+import model.http.CustomHttpResponse;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FrontController {
 
@@ -32,11 +40,39 @@ public class FrontController {
 
         CustomHttpResponse response;
         try {
-            response = (CustomHttpResponse) foundMethod.invoke(controller, request);
+            if (foundMethod.getParameterCount() == 1 && foundMethod.getParameters()[0].isAnnotationPresent(CustomRequestBody.class)) {
+                response = getResponseByRequest(foundMethod, request.getBody(), controller);
+            } else if (foundMethod.getParameterCount() == 1 && foundMethod.getParameters()[0].isAnnotationPresent(CustomRequestParams.class)) {
+                response = getResponseByRequest(foundMethod, request.getQuery(), controller);
+            } else {
+                response = (CustomHttpResponse) foundMethod.invoke(controller);
+            }
         } catch (Exception e) {
             throw new IllegalArgumentException("HttpResponse 생성에 실패했습니다.");
         }
         return response;
+    }
+
+    private CustomHttpResponse getResponseByRequest(Method method, CustomBaseHttpRequest request, BaseController controller) {
+        try {
+            Class target = method.getParameters()[0].getType();
+            Constructor constructor = target.getDeclaredConstructor(
+                    Arrays.stream(target.getDeclaredFields())
+                            .map(f -> f.getType())
+                            .collect(Collectors.toList())
+                            .toArray(new Class[0])
+            );
+            Field[] fields = target.getDeclaredFields();
+            return (CustomHttpResponse) method.invoke(
+                    controller,
+                    constructor.newInstance(Arrays.stream(fields)
+                            .map(f -> request.get(f.getName()))
+                            .collect(Collectors.toList())
+                            .toArray())
+            );
+        } catch (Exception e) {
+            throw new UnsupportedRequestException("HttpRequest 매핑에 실패했습니다.");
+        }
     }
 
 }
