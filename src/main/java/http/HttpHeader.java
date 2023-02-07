85c533dd50ba6d95f64e647e62ab10d698a6a2fc
Kloong1 <dkdlsmsm98@gmail.com>
2023-02-07 16:13:18 +0900

    feat: HTTP header 필드 이름을 상수로 갖고 있는 HttpHeader 추가

---
+++ src/main/java/http/HttpHeader.java
@@ -0,0 +1,7 @@
+package http;
+
+public class HttpHeader {
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String CONTENT_LENGTH = "Content-Length";
+    public static final String LOCATION = "Location";
+}
