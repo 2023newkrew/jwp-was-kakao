905f873c574128b1ef07332fb1d2e735d45b7e37
HONGMINJU <mhong0800@gmail.com>
2023-02-08 00:33:46 +0900

    refactor: @Getter, @AllArgsConstructor 제거

--- src/main/java/webserver/RequestHeader.java
+++ src/main/java/webserver/RequestHeader.java
@@ -1,15 +1,9 @@
 package webserver;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-
 import java.util.Map;
+import lombok.Builder;
 
-@Getter
 @Builder
-@AllArgsConstructor
 public class RequestHeader {
 
     private HttpMethod httpMethod;
@@ -22,14 +16,37 @@ public class RequestHeader {
 
     private Map<String, String> headers;
 
+    public RequestHeader(HttpMethod httpMethod, String url, Map<String, String> queryParams, String httpVersion,
+        Map<String, String> headers) {
+        this.httpMethod = httpMethod;
+        this.url = url;
+        this.queryParams = queryParams;
+        this.httpVersion = httpVersion;
+        this.headers = headers;
+    }
+
+    public HttpMethod getHttpMethod() {
+        return httpMethod;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Map<String, String> getQueryParams() {
+        return queryParams;
+    }
+
+    public String getHttpVersion() {
+        return httpVersion;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
 
     public void convertToAbsolutePath(ResourceType resourceType) {
-//        if (resourceType == ResourceType.STATIC) {
-//            url = resourceType.getPath() + url;//.substring(1);
-//            return;
-//        }
         url = resourceType.getPath() + url;
-
     }
 
     public boolean hasContentLength() {
