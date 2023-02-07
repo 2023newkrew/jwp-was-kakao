7f858e65be39217dc4f4aa08fed0d909bd30a7d5
Kloong1 <dkdlsmsm98@gmail.com>
2023-02-07 16:11:02 +0900

    feat: Content-Type 값을 상수로 가지고있는 HttpContentType 추가

---
+++ src/main/java/http/HttpContentType.java
@@ -0,0 +1,7 @@
+package http;
+
+public class HttpContentType {
+    public static final String TEXT_HTML = "text/html";
+    public static final String TEXT_CSS = "text/css";
+    public static final String TEXT_JAVASCRIPT = "text/javascript";
+}
