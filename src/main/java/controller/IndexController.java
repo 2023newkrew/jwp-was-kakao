cbcaeab16c5c77da73903f3e6b1605a9be5be1f7
SJGeeKo <sungjun.gee.ko@gmail.com>
2023-02-07 15:49:00 +0900

    refactor: IndexController에서 html의 파일 경로를 상수로 뽑아냄

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -14,13 +14,16 @@ import java.net.URISyntaxException;
  * 관련 URI: /index.html, /
  */
 public class IndexController extends Controller {
+
+    private static final String INDEX_FILE_PATH = "templates/index.html";
+
     @Override
     public void process(HttpRequest request, DataOutputStream dos) throws IOException {
         String reqMethod = request.getRequestHeader().get("method").orElseThrow(IllegalArgumentException::new);
 
         if (reqMethod.equals("GET")) {
             try {
-                byte[] returnBody = FileIoUtils.loadFileFromClasspath("templates/index.html");
+                byte[] returnBody = FileIoUtils.loadFileFromClasspath(INDEX_FILE_PATH);
 
                 if (returnBody == null) {
                     dos.writeBytes(ResponseHeader.of(HttpStatusCode.NOT_FOUND, ContentType.HTML).getValue());
