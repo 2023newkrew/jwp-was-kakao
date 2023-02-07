ce9b2dddb5e6686d7f6325c8e47d2b2665f72ae0
aaron.j <aaron.j@kakaocorp.com>
2023-02-07 18:33:24 +0900

    refactor: Map 초기화 시 사용된 double brace를 of 메소드 방식으로 대체

--- src/main/java/webserver/HandlerMapping.java
+++ src/main/java/webserver/HandlerMapping.java
@@ -3,13 +3,10 @@ package webserver;
 import webserver.handler.Handler;
 import webserver.handler.UserHandler;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class HandlerMapping {
-    private final Map<String, Handler> map = new HashMap<>() {{
-        put("/user/create", new UserHandler());
-    }};
+    private final Map<String, Handler> map = Map.of("/user/create", new UserHandler());
 
     public Handler getHandler(String uri) {
         return map.get(uri);
