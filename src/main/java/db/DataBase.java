ebff139d64a9fe9234541f62fc0f477fcb61cc22
soony.kwonn <soony.kwonn@kakaocorp.com>
2023-02-08 11:44:23 +0900

    refactor: Slf4j를 이용한 로깅으로 변경

--- src/main/java/db/DataBase.java
+++ src/main/java/db/DataBase.java
@@ -1,17 +1,19 @@
 package db;
 
+import lombok.extern.slf4j.Slf4j;
 import model.user.User;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 public class DataBase {
     private static Map<String, User> users = new HashMap<>();
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
-        System.out.println("Added " + user.getName());
+        log.info("Added " + user.getName());
     }
 
     public static User findUserById(String userId) {
