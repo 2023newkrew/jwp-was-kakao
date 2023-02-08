1c0d31e164ad59eff86130929ef0517404ff596c
jade.xyz <jade.xyz@kakaocorp.com>
2023-02-08 16:27:54 +0900

    fix: ResourceType의 확장자 목록을 Set으로 변경

--- src/main/java/webserver/ResourceType.java
+++ src/main/java/webserver/ResourceType.java
@@ -4,24 +4,22 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
+import java.util.Set;
 
 @RequiredArgsConstructor
 public enum ResourceType {
-    HTML("./templates", List.of("html", "ico")),
-    STATIC("./static", List.of("css", "js", "png", "eot", "svg", "ttf", "woff", "woff2")),
-    NONE("", Collections.emptyList());
+    HTML("./templates", Set.of("html", "ico")),
+    STATIC("./static", Set.of("css", "js", "png", "eot", "svg", "ttf", "woff", "woff2")),
+    NONE("", Collections.emptySet());
 
     private final String path;
-    private final List<String> extensions;
+    private final Set<String> extensions;
 
     public String getPath() {
         return path;
     }
 
     public static ResourceType getResourceType(String extension) {
-
-
         return Arrays.stream(ResourceType.values())
                 .filter(v -> v.extensions.contains(extension))
                 .findFirst()
