b2096394a35c7388e2a3afc29a51c34f578ee922
jayden.v <jayden.v@kakaocorp.com>
2023-02-08 16:52:14 +0900

    test: 통과되지 않는 테스트 수정

--- src/test/java/webserver/RequestHandlerTest.java
+++ src/test/java/webserver/RequestHandlerTest.java
@@ -4,14 +4,11 @@ import org.junit.jupiter.api.Test;
 import support.StubSocket;
 import utils.FileIoUtils;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RequestHandlerTest {
     @Test
-    void index() throws IOException, URISyntaxException {
+    void index() {
         // given
         final String httpRequest = String.join("\r\n",
                 "GET /index.html HTTP/1.1 ",
@@ -29,9 +26,10 @@ class RequestHandlerTest {
         // then
 
 
-        var expected = "HTTP/1.1 200 OK \r\n" +
-                "Content-Type: text/html;charset=utf-8 \r\n" +
-                "Content-Length: 6902 \r\n" +
+        var expected = "HTTP/1.1 200 OK\r\n" +
+                "Content-Length: 6902\r\n" +
+                "Location: /index.html\r\n" +
+                "Content-Type: text/html;charset=utf-8\r\n" +
                 "\r\n" +
                 new String(FileIoUtils.loadFileFromClasspath("templates/index.html"));
 
