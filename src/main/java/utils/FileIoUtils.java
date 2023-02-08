140c8c469b3c7463ff2f94e0b15f11363a08b391
jayden.v <jayden.v@kakaocorp.com>
2023-02-08 16:54:00 +0900

    feat: console이 아닌 logger를 통해 에러로그를 남기도록 수정

--- src/main/java/utils/FileIoUtils.java
+++ src/main/java/utils/FileIoUtils.java
@@ -1,16 +1,22 @@
 package utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileIoUtils {
+
+    private final static Logger logger = LoggerFactory.getLogger(FileIoUtils.class);
+
     public static byte[] loadFileFromClasspath(String filePath) {
         try {
             Path path = Paths.get(FileIoUtils.class.getClassLoader().getResource(filePath).toURI());
             return Files.readAllBytes(path);
         } catch (Throwable e) {
-            System.out.println("filePath = " + filePath);
+            logger.error("Cannot get resource from file path of \"{}\"" , filePath);
             e.printStackTrace();
             throw new RuntimeException(e);
         }
