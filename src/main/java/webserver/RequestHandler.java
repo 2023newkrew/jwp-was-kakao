0dc8a914af45799e332673188cd8eee8f3fba530
Nelfunction <oat641@gmail.com>
2023-02-08 19:02:25 +0900

    [eugene.641] HTTP 이해 - 웹 서버 구현 Step1 (#40)
    * feat : HttpMethod enum 생성
    
    * feat : HttpRequest 클래스 생성
    
    * feat : HttpRequestParser 클래스 추가 및 parse 메소드 구현
    
    * feat : RequestHandler 클래스 setBody 메소드 추가, run메소드 uri에 따른 body 생성 추가
    
    * feat : 테스트 index() 메소드 expected 수정
    
    * feat : build.gradle logger 관련 구현체 의존성 추가
    
    * feat : css, js, font, image 파일 지원
    
    * feat : HttpRequest body 멤버 추가, HttpRequestParser parse 메소드 body 추출 추가
    
    * feat : HttpRequest body 멤버변수 String 타입으로 변경
    
    * feat : HttpRequestParser parse 메소드내 http request의 body를 IOUtils.readData 메소드를 이용해 저장
    
    * feat : GetReqeustHandelr, PostRequestHandler 분리
    
    * feat : RequestHandelr 각 메소드별 body 획득 방식 변경
    
    * feat : DataBase users 멤버변수 HashMap으로 변경
    
    * feat : 회원가입시 post http request 전송
    
    * feat : RequestHandler 회원가입 POST 요청시 302 response 반환 및 redirection 구현
    
    * feat : RequestHandlerTest 수정
    
    * feat: Controller & HttpResponse 추가
    
    * refactor: Handler - Controller 분리
    
    ---------
    
    Co-authored-by: gkwodn1044 <gkwodn1044@gmail.com>

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,54 +2,81 @@ package webserver;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.IOUtils;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
 
     public void run() {
-        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
-
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello world".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+
+            HttpRequest httpRequest = getRequest(br);
+            HttpResponse httpResponse = new RequestController().requestMapping(httpRequest);
+
+            sendResponse(dos, httpResponse);
         } catch (IOException e) {
             logger.error(e.getMessage());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8 \r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + " \r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
+    public static HttpRequest getRequest(BufferedReader br) throws IOException {
+        // start line
+        String line = br.readLine();
+        String[] lineParts = line.split(" ");
+
+        HttpMethod httpMethod = HttpMethod.valueOf(lineParts[0]);
+        String uri = lineParts[1];
+        String httpVersion = lineParts[2];
+
+        // headers
+        Map<String, String> headers = new HashMap<>();
+        while ((line = br.readLine()) != null && line.length() > 0) {
+            int headerSeparatorIndex = line.indexOf(": ");
+            String headerName = line.substring(0, headerSeparatorIndex);
+            String headerValue = line.substring(headerSeparatorIndex + ": ".length());
+            headers.put(headerName, headerValue);
         }
+
+        // body
+        String body = "";
+        if (headers.containsKey("Content-Length")) {
+            body = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+        }
+
+        return new HttpRequest(httpMethod, uri, httpVersion, headers, body);
     }
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
+    private void sendResponse(DataOutputStream dos, HttpResponse httpResponse){
         try {
-            dos.write(body, 0, body.length);
+            // status line
+            dos.writeBytes(httpResponse.version + " " + httpResponse.status + " \r\n");
+            // header
+            for (String line: httpResponse.headers) {
+                dos.writeBytes(line);
+            }
+            // body
+            dos.write(httpResponse.body, 0, httpResponse.body.length);
+            // flush
             dos.flush();
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
+
 }
