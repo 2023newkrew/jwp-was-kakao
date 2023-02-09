eb4ed86923eea40800aa765075d13b6f1aeea417
jerrie.choi <pinkytime96@gmail.com>
2023-02-09 16:39:06 +0900

    refac: response의 body에 빈 문자열 추가

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -47,6 +47,7 @@ public class UserController extends BaseController {
     @CustomRequestMapping(url = "/user/login", httpMethod = CustomHttpMethod.POST)
     public CustomHttpResponse login(@CustomRequestBody User user) {
         CustomHttpHeader headers = new CustomHttpHeader();
+        User test = user;
         User loginUser = DataBase.findUserById(user.getUserId()).orElseThrow(() -> new UserNotFoundException("아이디와 비밀번호가 일치하지 않습니다."));
         if (loginUser.hasSamePassword(user.getPassword())) {
             headers.put("Set-Cookie", new CustomHttpCookie().getCookie());
@@ -54,12 +55,14 @@ public class UserController extends BaseController {
             return new CustomHttpResponse.Builder()
                     .httpStatus(CustomHttpStatus.FOUND)
                     .headers(headers)
+                    .body("")
                     .build();
         }
         headers.put("Location", "/user/login_failed.html");
         return new CustomHttpResponse.Builder()
                 .httpStatus(CustomHttpStatus.FOUND)
                 .headers(headers)
+                .body("")
                 .build();
     }
 
