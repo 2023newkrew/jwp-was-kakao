51be633520d2029f8ac2689cf12df0f99a89333c
jungin0507 <a01033046758@gmail.com>
2023-02-09 15:51:56 +0900

    feat: 로그인 기능 재활성화
    (cherry picked from commit 62e46777187f3c0bc4055774ea235e1fc47fd266)

--- src/main/java/webserver/handler/posthandler/impl/LoginRequestHandler.java
+++ src/main/java/webserver/handler/posthandler/impl/LoginRequestHandler.java
@@ -1,10 +1,19 @@
 package webserver.handler.posthandler.impl;
 
+import db.DataBase;
+import model.User;
+import webserver.constant.HttpHeaderProperties;
+import webserver.constant.HttpStatus;
 import webserver.handler.posthandler.PostRequestHandler;
 import webserver.request.HttpRequest;
+import webserver.request.QueryParams;
 import webserver.response.HttpResponse;
 
 public class LoginRequestHandler extends PostRequestHandler {
+
+    private static final String LOGIN_SUCCESS_URL = "http://localhost:8080/index.html";
+
+    private static final String LOGIN_FAILED_URL = "http://localhost:8080/user/login_failed.html";
 
     private LoginRequestHandler() {
     }
@@ -27,22 +36,17 @@ public class LoginRequestHandler extends PostRequestHandler {
 
     @Override
     public void handle(HttpRequest request, HttpResponse.Builder responseBuilder) {
-//        QueryParams queryParams = new QueryParams(request.getBody());
-//        String userId = queryParams.get("userId");
-//        String password = queryParams.get("password");
-//        User user = DataBase.findUserById(userId);
-//        if (user.hasSamePassword(password)) {
-//            return new HttpResponse.Builder()
-//                    .setStatus(HttpStatus.FOUND)
-//                    .addHeader("Set-Cookie", UUID.randomUUID()
-//                            .toString())
-//                    .addHeader(HttpHeaderProperties.LOCATION.getKey(), "http://localhost:8080/index.html")
-//                    .build();
-//        } else {
-//            return new HttpResponse.Builder()
-//                    .setStatus(HttpStatus.FOUND)
-//                    .addHeader(HttpHeaderProperties.LOCATION.getKey(), "http://localhost:8080/login_failed.html")
-//                    .build();
-//        }
+        QueryParams queryParams = new QueryParams(request.getBody());
+        String userId = queryParams.get("userId");
+        String password = queryParams.get("password");
+        User user = DataBase.findUserById(userId);
+        String redirectUrl = LOGIN_FAILED_URL;
+        if (user != null && user.hasSamePassword(password)) {
+            redirectUrl = LOGIN_SUCCESS_URL;
+            request.getSession()
+                    .setAttribute("user", user);
+        }
+        responseBuilder.setStatus(HttpStatus.FOUND)
+                .addHeader(HttpHeaderProperties.LOCATION.getKey(), redirectUrl);
     }
 }
