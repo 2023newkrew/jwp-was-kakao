ecad12d8116b6bf45293e0a0827a15395fb8494f
jungin0507 <a01033046758@gmail.com>
2023-02-09 15:31:21 +0900

    feat: 쿠키 파싱 로직 구현
    (cherry picked from commit 771af5689e600285a2ef54a5ebb5402fd2e8b682)

---
+++ src/main/java/webserver/Cookie.java
@@ -0,0 +1,33 @@
+package webserver;
+
+import com.github.jknack.handlebars.internal.lang3.StringUtils;
+import lombok.Getter;
+
+@Getter
+public class Cookie {
+
+    private final String name;
+
+    private final String value;
+
+    public Cookie(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public Cookie(String cookie) {
+        String[] cookieParts = cookie.split("=");
+        this.name = cookieParts[0].trim();
+        this.value = cookieParts[1].trim();
+    }
+
+    @Override
+    public String toString() {
+        return StringUtils.join(
+                name,
+                "=",
+                value,
+                "; Path=/"
+        );
+    }
+}
--- src/main/java/webserver/request/HttpRequest.java
+++ src/main/java/webserver/request/HttpRequest.java
@@ -2,10 +2,15 @@ package webserver.request;
 
 import lombok.Getter;
 import utils.IOUtils;
+import webserver.Cookie;
 import webserver.constant.HttpHeaderProperties;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class HttpRequest {
@@ -14,12 +19,30 @@ public class HttpRequest {
 
     private HttpRequestHeaders headers;
 
+    private Session session;
+
+    private List<Cookie> cookies = new ArrayList<>();
+
     private String body;
 
     public HttpRequest(BufferedReader reader) throws IOException {
         this.target = new HttpRequestTarget(reader);
         this.headers = new HttpRequestHeaders(reader);
+        if (headers.containsKey("Cookie")) {
+            parseCookie(headers.get("Cookie"));
+        }
         parseBody(reader);
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+
+    private void parseCookie(String cookies) {
+        this.cookies = Arrays.stream(cookies.split(";"))
+                .map(String::trim)
+                .map(Cookie::new)
+                .collect(Collectors.toList());
     }
 
     private void parseBody(BufferedReader reader) throws IOException {
