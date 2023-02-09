3bdc9bccfbd4089393732305efac1a6fdec7c91c
Sangyups <feels4637@gmail.com>
2023-02-09 15:57:23 +0900

    feat: 유저 리스트 반환 기능 추가

---
+++ src/main/java/webserver/controller/UserListController.java
@@ -0,0 +1,47 @@
+package webserver.controller;
+
+import static webserver.controller.UserLoginController.REDIRECTION_FAILED;
+
+import db.SessionStorage;
+import db.UserStorage;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import model.MyHttpRequest;
+import model.MyHttpResponse;
+import model.MyModelAndView;
+import model.Session;
+import org.springframework.http.HttpStatus;
+import webserver.Controller;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserListController implements Controller {
+
+    public static final String URL = "/user/list.html";
+
+    private static final UserListController INSTANCE = new UserListController();
+
+    public static UserListController getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public MyModelAndView process(MyHttpRequest httpRequest, MyHttpResponse httpResponse) {
+        if (!doAuthenticate(httpRequest)) {
+            httpResponse.setStatus(HttpStatus.TEMPORARY_REDIRECT);
+            httpResponse.setLocation(REDIRECTION_FAILED);
+            return new MyModelAndView();
+        }
+
+        httpResponse.setStatus(HttpStatus.OK);
+
+        MyModelAndView mav = new MyModelAndView("./templates", httpRequest.getUrl());
+        mav.addAttribute("users", UserStorage.findAll());
+        return mav;
+    }
+
+    private boolean doAuthenticate(MyHttpRequest httpRequest) {
+        String sessionId = httpRequest.getSession();
+        Session session = SessionStorage.findSession(sessionId);
+        return session.getAttribute("user") != null;
+    }
+}
