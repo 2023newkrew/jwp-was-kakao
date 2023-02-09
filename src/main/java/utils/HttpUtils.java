c3425ed3bd56d1948fc5e073661138770db68f3b
jerrie.choi <pinkytime96@gmail.com>
2023-02-09 12:23:43 +0900

    refac: RequestLine으로 수정

--- src/main/java/utils/HttpUtils.java
+++ src/main/java/utils/HttpUtils.java
@@ -1,6 +1,5 @@
 package utils;
 
-import exception.EmptyHeaderException;
 import model.http.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,29 +13,14 @@ import java.util.stream.Collectors;
 
 public class HttpUtils {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-    private static final String LINE_DELIMITER = " ";
-    private static final String URL_DELIMITER = "\\?";
-    private static final String QUERY_DELIMITER = "&";
-    private static final String KEY_VALUE_DELIMITER = "=";
+    public static final String QUERY_DELIMITER = "&";
+    public static final String KEY_VALUE_DELIMITER = "=";
     private static final String HEADER_DELIMITER = ": ";
     private static final String CONTENT_LENGTH_HEADER = "Content-Length";
 
     public static CustomHttpRequest createHttpRequest(BufferedReader br) throws IOException {
         String line = br.readLine();
-        validateLine(line);
-        String[] tokens = line.split(LINE_DELIMITER);
-
-        CustomHttpMethod customHttpMethod = CustomHttpMethod.from(tokens[0]);
-
-        String[] urls = tokens[1].split(URL_DELIMITER);
-        String url = urls[0];
-
-        CustomHttpRequestQuery query = null;
-        if (hasQuery(urls)) {
-            query = getQuery(urls[1]);
-        }
-
-        String protocol = tokens[2];
+        CustomHttpRequestLine httpRequestLine = new CustomHttpRequestLine(line);
 
         line = br.readLine();
         CustomHttpHeader headers = new CustomHttpHeader();
@@ -52,7 +36,7 @@ public class HttpUtils {
             body = getBody(IOUtils.readData(br, Integer.parseInt(contentLength)));
         }
 
-        return new CustomHttpRequest(customHttpMethod, url, query, protocol, headers, body);
+        return new CustomHttpRequest(httpRequestLine, headers, body);
     }
 
     public static void respond(DataOutputStream dos, CustomHttpResponse response){
@@ -65,22 +49,6 @@ public class HttpUtils {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
-    }
-
-    private static void validateLine(String line) {
-        if (line == null || line.isEmpty()) {
-            throw new EmptyHeaderException("잘못된 요청 형식입니다.");
-        }
-    }
-
-    private static boolean hasQuery(String[] urls) {
-        return urls.length > 1;
-    }
-
-    private static CustomHttpRequestQuery getQuery(String queryString) {
-        return new CustomHttpRequestQuery(Arrays.stream(queryString.split(QUERY_DELIMITER))
-                .map(field -> field.split(KEY_VALUE_DELIMITER))
-                .collect(Collectors.toMap(f -> f[0], f -> f[1])));
     }
 
     private static CustomHttpRequestBody getBody(String requestBody) {
