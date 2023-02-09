3a211928729872dc99b4a936dc24ab0a598c24db
SJGeeKo <sungjun.gee.ko@gmail.com>
2023-02-09 16:50:28 +0900

    feat: Cookie 없이 들어온 요청에 대해 Set-Cookie 응답기능 구현

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,8 +2,10 @@ package controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import webserver.HttpCookie;
 import webserver.HttpRequest;
 import webserver.HttpResponse;
+import webserver.RequestHeader;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -13,7 +15,8 @@ public abstract class Controller {
     static final Logger logger = LoggerFactory.getLogger(Controller.class);
 
     public void process(HttpRequest request, HttpResponse response, DataOutputStream dos) throws IOException {
-        String method = request.getRequestHeader().get("method").orElseThrow(IllegalArgumentException::new);
+        RequestHeader requestHeader = request.getRequestHeader();
+        String method = requestHeader.get("method").orElseThrow(IllegalArgumentException::new);
         if (method.equals("GET")) {
             doGet(request, response, dos);
         }
@@ -21,6 +24,14 @@ public abstract class Controller {
             doPost(request, response, dos);
         }
         doFinally(request, response, dos);
+
+        // 요청 헤더에 쿠키가 없다면 생성하여 응답 전송
+        // TODO: Optional 활용 개성방안?
+        HttpCookie requestCookie = new HttpCookie(requestHeader.get("Cookie").orElse(""));
+
+        if (requestCookie.get("JSESSIONID").isEmpty()) {
+            response.getResponseHeader().setCookie(new HttpCookie());
+        }
         sendResponse(response, dos);
     }
 
