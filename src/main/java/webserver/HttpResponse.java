d6384d8c9aedcc154b80245522db197971f5ad05
SJGeeKo <sungjun.gee.ko@gmail.com>
2023-02-09 15:51:49 +0900

    feat: HttpResponse 클래스 구현

---
+++ src/main/java/webserver/HttpResponse.java
@@ -0,0 +1,23 @@
+package webserver;
+
+public class HttpResponse {
+
+    private ResponseHeader responseHeader;
+    private byte[] responseBody;
+
+    public ResponseHeader getResponseHeader() {
+        return responseHeader;
+    }
+
+    public byte[] getResponseBody() {
+        return responseBody;
+    }
+
+    public void setResponseHeader(ResponseHeader requestHeader) {
+        this.responseHeader = requestHeader;
+    }
+
+    public void setResponseBody(byte[] responseBody) {
+        this.responseBody = responseBody;
+    }
+}
