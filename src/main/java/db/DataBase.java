81af9d49c5310b92d9d4a836a0a525676b3ab335
정핸모 <gusah009@naver.com>
2023-02-09 16:16:50 +0900

    chore: 불필요한 데이터 제거

--- src/main/java/db/DataBase.java
+++ src/main/java/db/DataBase.java
@@ -10,11 +10,6 @@ public class DataBase {
 
     private static final Map<String, User> users = new HashMap<>();
 
-    static {
-        users.put("cu", new User("cu", "asdf", "dfsa", "fdsaf@fd.scom"));
-        users.put("bsd", new User("bsd", "asdf", "dfsa", "fdsaf@fd.scom"));
-    }
-
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
     }
