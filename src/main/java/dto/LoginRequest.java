72e60172593263a2a7a2e684b9ea695b986065bc
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-02-10 13:58:12 +0900

    refactor: LoginRequest 필드에 대한 Null 여부 체크 추가

--- src/main/java/dto/LoginRequest.java
+++ src/main/java/dto/LoginRequest.java
@@ -11,10 +11,15 @@ import java.util.Map;
 @Builder(access = AccessLevel.PRIVATE)
 @Getter
 public class LoginRequest {
+    private static final String USER_ID = "userId";
+    private static final String PASSWORD = "password";
     private final String userId;
     private final String password;
 
     public static LoginRequest of(Map<String, String> queryString) {
+        if (!queryString.containsKey(USER_ID) || !queryString.containsKey(PASSWORD)) {
+            throw new IllegalArgumentException();
+        }
         return LoginRequest.builder()
                 .userId(queryString.get("userId"))
                 .password(queryString.get("password"))
