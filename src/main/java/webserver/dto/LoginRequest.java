9fc2d2d51807cf239e1081a66b4685cf5731ab2d
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-02-13 17:51:37 +0900

    refactor: LoginRequest 생성자에서 인자값 Null 여부 체크

--- src/main/java/webserver/dto/LoginRequest.java
+++ src/main/java/webserver/dto/LoginRequest.java
@@ -3,11 +3,9 @@ package webserver.dto;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.util.Map;
 
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder(access = AccessLevel.PRIVATE)
 @Getter
 public class LoginRequest {
@@ -16,10 +14,16 @@ public class LoginRequest {
     private final String userId;
     private final String password;
 
-    public static LoginRequest of(Map<String, String> queryString) {
-        if (!queryString.containsKey(USER_ID) || !queryString.containsKey(PASSWORD)) {
+    private LoginRequest(String userId, String password) {
+        if (userId == null || password == null ||
+                userId.isEmpty() || password.isEmpty()) {
             throw new IllegalArgumentException();
         }
+        this.userId = userId;
+        this.password = password;
+    }
+
+    public static LoginRequest of(Map<String, String> queryString) {
         return LoginRequest.builder()
                 .userId(queryString.get("userId"))
                 .password(queryString.get("password"))
